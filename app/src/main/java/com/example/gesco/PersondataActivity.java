0537617dcf2a5b2ff3f45a5fca7d325861efea7c
Muhammed Sacuar Hussain <imsacuar01@yahoo.com>
2021-01-25 13:03:01 +0100

    Gesca care

---
+++ app/src/main/java/com/example/gesco/PersondataActivity.java
@@ -0,0 +1,14 @@
+package com.example.gesco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersondataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persondata);
+    }
+}
